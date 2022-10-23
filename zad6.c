7d1d6ea6d65a28c9c666d9884d9e772fa90d643e
kmkacpermajor <kmkacpermajor@gmail.com>
2022-10-23 21:01:29 +0200

    pop zad6

--- zad6.c
+++ zad6.c
@@ -2,46 +2,48 @@
 #include<unistd.h>
 #include<fcntl.h>
 
-int main(int argc, char* argv[]){
-    int fd = open(argv[1], O_RDONLY);
-    int fd2 = open(argv[2], O_WRONLY | O_CREAT, 0666);
-
+void pom(int fdr, int fdw, int tryb){
+    char buf[1024];
     char a;
     int x;
     int licznik = 0;
+    int i=0;
 
-    lseek(fd, 0, SEEK_SET);
+    lseek(fdr, -1, SEEK_END);
 
-    while(x = read(fd, &a, 1)>0){
-        read(fd, &a, 1);
-        write(fd2, &a, 1);
-        licznik++;
-        if (licznik == 10) break;
-    }
+    while(x = read(fdr, &a, 1)>0){
+        buf[i] = a;
 
-    licznik = 0;
-
-    lseek(fd, 0, SEEK_SET);
-
-    write(fd2, "\n", 1);
-    while(x = read(fd, &a, 1)>0){
-        write(fd2, &a, 1);
-        if(a=='\n' || a==' '){
-            licznik += 1;
+        if (tryb == 0){
+            licznik++;
+        }else if (tryb == 1 || a=='\n' || a==' ') {
+            licznik ++;
+        }else if (tryb == 2 || a=='\n'){
+            licznik++;
         }
-        if(licznik==10) break;
-    }
 
-    licznik = 0;
-
-    lseek(fd, 0, SEEK_SET);
-
-    write(fd2, "\n", 1);
-    while(x = read(fd, &a, 1)>0){
-        write(fd2, &a, 1);
-        if(a=='\n'){
-            licznik += 1;
+        if (licznik == 10){
+            for(int j = i; j>=0; j--) 
+                write(fdw, &buf[j], 1);
+            break;
         }
-        if(licznik==10) break;
+        lseek(fdr, -2, SEEK_CUR);
+        i++;
     }
+
+    lseek(fdr, 0, SEEK_SET);
+    write(fdw, "\n", 1);
+}
+
+int main(int argc, char* argv[]){
+    int fdr = open(argv[1], O_RDONLY);
+    int fdw = open(argv[2], O_WRONLY | O_CREAT, 0666);
+
+    pom(fdr, fdw, 0);
+
+    pom(fdr, fdw, 1);
+
+    pom(fdr, fdw, 2);
+
+    return 0;
 }
