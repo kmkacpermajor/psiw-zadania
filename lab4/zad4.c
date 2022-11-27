d86b1a404652f35ea34d2032fecfd869a1f160a1
kmkacpermajor <kmkacpermajor@gmail.com>
2022-11-27 21:59:09 +0100

    poprawka

--- lab4/zad4.c
+++ lab4/zad4.c
@@ -5,31 +5,34 @@ int main(){
     int fd1[2];
     int fd2[2];
     int fd3[2];
-    int buf[4];
-
+    int buf[4];    
+    
     pipe(fd1);
     pipe(fd2);
     pipe(fd3);
-    
+
     if(fork()!=0){
-        close(fd1[0]);
-        for(int i=0; i<4; i++){
-            buf[i] = i+1;
-            printf("%d ", buf[i]);
+        if(fork()==0){
+            close(fd1[0]);
+            for(int i=0; i<4; i++){
+                buf[i] = i+1;
+                printf("%d ", buf[i]);
+            }
+            printf("\n");
+            write(fd1[1], buf, sizeof(buf));
+            close(fd1[1]);
+        }else{
+            close(fd1[1]);
+            close(fd2[0]);
+            read(fd1[0], buf, sizeof(buf));
+            close(fd1[0]);
+            for(int i=0; i<4; i++){
+                buf[i]++;
+            }
+            write(fd2[1], buf, sizeof(buf));
+            close(fd2[1]);
         }
-        printf("\n");
-        write(fd1[1], buf, sizeof(buf));
-        close(fd1[1]);
     }else{
-        close(fd1[1]);
-        close(fd2[0]);
-        read(fd1[0], buf, sizeof(buf));
-        close(fd1[0]);
-        for(int i=0; i<4; i++){
-            buf[i]++;
-        }
-        write(fd2[1], buf, sizeof(buf));
-        close(fd2[1]);
         if(fork()==0){
             close(fd3[0]);
             close(fd2[1]);
@@ -51,4 +54,5 @@ int main(){
             printf("\n");
         }
     }
+
 }
